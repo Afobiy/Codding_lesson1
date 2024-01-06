6a1a0d65500ac987a60786376251604eb07e8e1e
Afobiy <deadseller@mail.ru>
2024-01-06 20:46:14 +0300

    Задание 3

---
+++ Desktop/Coding_lessons/Homework1/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число от 10 до 99");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 10)
+{
+    Console.WriteLine("Должнобыть число от 10 до 99");
+    return;
+}
+
+if(num > 99)
+{
+    Console.WriteLine("Должнобыть число от 10 до 99");
+    return;
+}
+
+int a = num / 10;
+int b = num % 10;
+
+
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
