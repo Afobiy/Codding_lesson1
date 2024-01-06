8d61143d6ac9f1e7eb18cbdc225a37028a5b81c7
Afobiy <deadseller@mail.ru>
2024-01-06 20:43:30 +0300

    Задание 1

---
+++ Desktop/Coding_lessons/Homework1/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿long custom_number1 = Convert.ToInt32(Console.ReadLine());
+long checker = 7*23;
+
+long reminder = custom_number1 % checker; 
+
+if(reminder == 0)
+{
+  Console.WriteLine("Делится на 23 и на 7");  
+}
+else
+{
+    Console.WriteLine("Не делится");
+}
+
