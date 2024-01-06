ae3bde89309356b5b2a38aacf12ea6d504e660ee
Afobiy <deadseller@mail.ru>
2024-01-06 20:46:39 +0300

    Задание 4

---
+++ Desktop/Coding_lessons/Homework1/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Metrics;
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i;
+do
+{
+    i = num % 10;
+    num = num / 10;
+    Console.Write(i + ", "); 
+}
+while(num > 0);
