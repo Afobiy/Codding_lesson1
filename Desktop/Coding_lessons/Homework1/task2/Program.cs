abff3ba41667ea3387cc6ed55508669250ccbac3
Afobiy <deadseller@mail.ru>
2024-01-06 20:45:29 +0300

    Задание 2

---
+++ Desktop/Coding_lessons/Homework1/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ведите координат X, не равный 0 ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите координат Y, не равный 0 ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if(X > 0)
+{
+    if(Y > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Четвёртая четверть");
+    }
+}
+else
+{
+    if(Y > 0)
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Третья четверть");
+    }
+}
