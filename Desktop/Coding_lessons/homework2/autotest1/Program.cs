c2f7375b1a9667da53e44bea3ffbb24a7b7db92a
Afobiy <deadseller@mail.ru>
2024-01-17 20:16:38 +0300

    task1 autotest

---
+++ Desktop/Coding_lessons/homework2/autotest1/Program.cs
@@ -0,0 +1,17 @@
+﻿ int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+        int minRange = 10;
+        int maxRange = 90;
+        int i = 0;
+        int j = 0;
+        
+        while(i < array.Length)
+      if(array[i] >= minRange & array[i] <= maxRange)
+      {
+        i = i + 1;
+        j = j + 1; 
+      }
+      else
+      {
+        i = i + 1;//Введите сюда свое решение
+    }
+Console.WriteLine(j);
