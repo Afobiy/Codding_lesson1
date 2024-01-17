447eb1deceb29fe36c63a1ef5859978ac4bdea54
Afobiy <deadseller@mail.ru>
2024-01-17 18:53:43 +0300

    task1

---
+++ Desktop/Coding_lessons/homework2/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {10, 10, 10, 2, 4, 1, 9, 4, 8, 50};
+int min = 20;
+int max = 90;
+int i = 0;
+int j = 0;
+while (i < array.Length)
+{
+    if (array[i] > min & array[i] < max)
+    {
+        Console.WriteLine(array[i]);
+        j = j + 1;
+        i = i + 1;
+    }
+    else
+    {
+    i = i + 1;
+    }
+}
+Console.WriteLine(j);
