beefa41dd5af7eb377b02a6ee26e92f0f4acc687
Afobiy <deadseller@mail.ru>
2024-01-17 21:49:23 +0300

    task3 autotest

---
+++ Desktop/Coding_lessons/homework2/autotest3/Program.cs
@@ -0,0 +1,35 @@
+﻿double[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; 
+
+int minCounter = 0;
+int maxCounter = 0;
+double min = array[minCounter];
+double max = array[maxCounter];
+
+while(minCounter < array.Length)
+{
+    if(min >= array[minCounter])
+    {
+        min = array[minCounter];
+        minCounter = minCounter + 1;
+    }
+    else
+    {
+        minCounter = minCounter + 1;
+    }
+}
+while(maxCounter < array.Length)
+{
+    if(max <= array[maxCounter])
+    {
+        max = array[maxCounter];
+        maxCounter = maxCounter + 1;
+    }
+    else
+    {
+        maxCounter = maxCounter + 1;
+    }
+}
+double difference = max - min;
+Console.WriteLine(min);
+Console.WriteLine(max);
+Console.WriteLine(difference);
