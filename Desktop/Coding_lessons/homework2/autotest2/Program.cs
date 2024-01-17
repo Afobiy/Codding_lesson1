8b12493f413cf9e806759388513a4e1705af8a0a
Afobiy <deadseller@mail.ru>
2024-01-17 20:46:16 +0300

    task2 autotest

---
+++ Desktop/Coding_lessons/homework2/autotest2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {2, 5, 2, 3, 6, 3, 7, 8, 2, 4};
+int i = 0;
+int j = 0;
+while (i < array.Length)
+{
+    if(array[i] % 2 == 0)
+    {
+        j = j + 1;
+        i = i + 1;
+    }
+    else
+    {
+        i = i + 1;
+    }
+}
+Console.WriteLine(j);
