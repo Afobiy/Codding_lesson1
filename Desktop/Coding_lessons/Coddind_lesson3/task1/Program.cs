3e18b7b56865300b611e1ac65953ef92b92c3a76
Afobiy <deadseller@mail.ru>
2024-01-18 16:46:11 +0300

    task1 funtion'

---
+++ Desktop/Coding_lessons/Coddind_lesson3/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denuminator = c + d;
+    double result = numinator / denuminator;
+    return result;
+}
+
+CalculateFormula(1, 2, 3, 4);
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
