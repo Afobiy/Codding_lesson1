f928be77e3877e5125625afc4ddaf37ba7325f3f
Afobiy <deadseller@mail.ru>
2024-01-20 14:00:24 +0300

    task3 without methods

---
+++ Desktop/Coding_lessons/Homework3/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] array = { 1, 3, 5, 6, 7, 8 };
+int i = 0;
+int iNeg = array.Length - 1;
+int iNegInMemory = 0;
+int iInMemory = 0;
+while (i < array.Length)
+{
+    if (iNeg > i)
+    {
+        iInMemory = array[i];
+        iNegInMemory = array[iNeg];
+        array[i] = iNegInMemory;
+        array[iNeg] = iInMemory;
+        Console.WriteLine(array[i]);
+        i++;
+        iNeg--;
+    }
+    else
+    {
+        Console.WriteLine(array[i]);
+        i++;
+        iNeg--;
+    }
+}
