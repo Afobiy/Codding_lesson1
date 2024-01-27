393d9ace4c337f91622a3e4b095e810dbea10b20
Afobiy <deadseller@mail.ru>
2024-01-27 17:14:16 +0300

    task2

---
+++ Desktop/Coding_lessons/homework5Strings/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string mixedCaseLettes = "QWerTyuIO";
+
+string GoToLowCase(string mixedCaseLettes)
+{
+    string newString = "";
+    for (int i = 0; i < mixedCaseLettes.Length; i++) ;
+    newString = mixedCaseLettes.ToLower();
+    return newString;
+}
+
+string result = GoToLowCase(mixedCaseLettes);
+Console.WriteLine(result);
