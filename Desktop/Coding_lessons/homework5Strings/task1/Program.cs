6009ccd90c2fde4f9d055acbf06f78c433ec5915
Afobiy <deadseller@mail.ru>
2024-01-27 12:19:56 +0300

    task1

---
+++ Desktop/Coding_lessons/homework5Strings/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+char[,] lettets =
+{
+    {'Q','w','e','r'},
+    {'a','s','d','f'},
+    {'z','x','c','v'}
+
+};
+
+string GetString(char[,] letters)
+{
+    string words = "";
+    for (int y = 0; y < letters.GetLength(0); y++)
+    {
+        for (int x = 0; x < letters.GetLength(1); x++)
+        {
+            words += letters[y, x];
+        }
+    }return words;
+}
+
+
+string words=GetString(lettets);
+Console.Write(words);
