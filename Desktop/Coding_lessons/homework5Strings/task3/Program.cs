187d7c381987636879c8b99573d642cc9e519633
Afobiy <deadseller@mail.ru>
2024-01-27 18:05:50 +0300

    task3

---
+++ Desktop/Coding_lessons/homework5Strings/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите слово");
+string stringForCheck = Console.ReadLine();
+//bool palindromeChercker;
+
+bool CheckForPalindrome(string stringForCheck)
+{
+bool palindromeChercker = true;
+for (int i = 0; i < stringForCheck.Length / 2; i++)
+{
+    if(stringForCheck[i] == stringForCheck[(stringForCheck.Length -1)-i])
+    {
+        palindromeChercker = true;
+    }
+    else
+    {
+        palindromeChercker = false;
+        break;
+    }
+} 
+return palindromeChercker;
+}
+
+bool palindromeChercker = CheckForPalindrome(stringForCheck);
+if(palindromeChercker == true)
+{
+    Console.WriteLine("Это слово палиндром");
+}
+else
+{
+    Console.WriteLine("Это слово не палиндром");
+
+}
