523e0730a5efc938b0e9ae15c35a219d1db0015e
Afobiy <deadseller@mail.ru>
2024-02-11 23:19:49 +0300

    task2

---
+++ Desktop/Coding_lessons/Codding_practice5_recursion/task2Acerman/Program.cs
@@ -0,0 +1,31 @@
+﻿//         n+1,             m=0;
+//A(m,n) = A(m-1,1)         m>0,n=0;
+//         A(m-1, A(m,n-1)) m>0,n>0
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+int Acerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    if (n == 0)
+    {
+        return Acerman(m - 1, 1);
+    }
+    else
+    {
+        return Acerman(m - 1, Acerman(m, n - 1));
+    }
+}
+
+
+
+Console.Write("Функция Акермана равно " + Acerman(m, n));
