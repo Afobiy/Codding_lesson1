489fe7a47a4ece1e85e006a44ffd831f752dac3b
Afobiy <deadseller@mail.ru>
2024-02-11 23:16:15 +0300

    task3

---
+++ Desktop/Coding_lessons/Codding_practice5_recursion/task3Array/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = { 1, 2, 3, 4, 5, 6, 7};
+int i = 0;
+
+void ReverseArray(int[] arr)
+{
+    if (i < arr.Length - arr.Length / 2)
+    {
+        int dump = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = dump;
+        i++;
+        ReverseArray(arr);
+    }
+}
+
+
+
+ReverseArray(arr);
+for (i = 0; i <= arr.Length - 1; i++)
+{
+    Console.Write(arr[i] + " ");
+}
