a00cf70b55f7a40a556de6a0fe0332ff17a37249
Afobiy <deadseller@mail.ru>
2024-02-11 23:20:50 +0300

    task1

---
+++ Desktop/Coding_lessons/Codding_practice5_recursion/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("введите первую границу интервала m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите вторую границу интервала n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+if (m > n)
+{
+    Console.WriteLine(" m должна быть меньше n");
+    return;
+}
+
+void ShowNaturals(int i)
+{
+    if (i <= n)
+    {
+        if (i > 0)
+        {
+            Console.Write(i + " ");
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+        ShowNaturals(i);
+    }
+
+}
+ShowNaturals(m);
